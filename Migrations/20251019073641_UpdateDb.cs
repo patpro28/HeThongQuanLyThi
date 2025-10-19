eb00746a1b94cd8f91d5ced67351fbc04745a4e6
patpro28 <phamanhtu1996@gmail.com>
2025-10-19 14:37:38 +0700

    Remove Point Field in Problem

---
+++ Migrations/20251019073641_UpdateDb.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HeThongQuanLyThi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Points",
+                table: "Problems");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Points",
+                table: "Problems",
+                type: "decimal(9,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
